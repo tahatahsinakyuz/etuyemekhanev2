d4e06218c447d6c5ca6884c7f3343c4e66ecd16a
tahatahsinakyuz <takyuz515@gmail.com>
2025-05-24 11:15:15 +0300

    Statik dosya servisi ve index yönlendirme eklendi

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const path = require('path');
 const mysql = require('mysql2/promise');
 const bodyParser = require('body-parser');
 const cors = require('cors');
